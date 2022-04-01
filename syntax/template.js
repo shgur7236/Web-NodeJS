ecd87cbbbd9b4f131ea0da63448ff20b01a08f32
shgur7236 <shgurtns7236@naver.com>
2022-04-01 16:55:40 +0900

    template

---
+++ syntax/template.js
@@ -0,0 +1,8 @@
+var name = 'Nh';
+var letter = 'Dear '+name+'\n\nihqidhiqhi qhqiodhqwdho'+name+'sosososo adhcn';
+//console.log(letter);
+
+var letter = `Dear ${name}
+
+ihqidhiqhi ${1+1}qhqiodhqwdho. ${name}sosososo adhcn`;
+console.log(letter);
