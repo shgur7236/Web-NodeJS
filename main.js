f4c68f0c2445767beca7a91747f904663239642f
shgur7236 <shgurtns7236@naver.com>
2022-04-02 17:53:22 +0900

    파일목록 알아내기

--- main.js
+++ main.js
@@ -9,8 +9,18 @@ var app = http.createServer(function(request,response){
 
     if(pathname === '/'){
         if(queryData.id === undefined){
+
+            fs.readdir('./data', function(error,filelist){
                 var description = 'Hello node.js';
                 var title = 'Welcome';
+                var list = '<ul>';
+                var i = 0;
+                while(i < filelist.length){
+                    list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                    i = i + 1;
+                }
+
+                list = list+'</ul>';
                 var template = `
                 <!doctype html>
                 <html>
@@ -20,11 +30,7 @@ var app = http.createServer(function(request,response){
                 </head>
                 <body>
                 <h1><a href="/">WEB</a></h1>
-                <ul>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ul>
+                ${list}
                 <h2>${title}</h2>
                 <p>${description}</p>
                 </body>
@@ -32,7 +38,17 @@ var app = http.createServer(function(request,response){
                 `;
                 response.writeHead(200);
                 response.end(template);
+            })
         } else {
+            fs.readdir('./data', function(error,filelist){
+                var description = 'Hello node.js';
+                var title = 'Welcome';
+                var list = '<ul>';
+                var i = 0;
+                while(i < filelist.length){
+                    list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                    i = i + 1;
+                }
             fs.readFile(`data/${queryData.id}`,'utf8', function(err, description){
                 var title = queryData.id;
                 var template = `
@@ -44,11 +60,7 @@ var app = http.createServer(function(request,response){
                 </head>
                 <body>
                 <h1><a href="/">WEB</a></h1>
-                <ul>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ul>
+                ${list}
                 <h2>${title}</h2>
                 <p>${description}</p>
                 </body>
@@ -57,6 +69,7 @@ var app = http.createServer(function(request,response){
                 response.writeHead(200);
                 response.end(template);
             });
+        });
         }
         
     } else {
