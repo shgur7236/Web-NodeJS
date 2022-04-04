585637c378971fa379d55ad8f8281730df321bad
shgur7236 <shgurtns7236@naver.com>
2022-04-04 20:08:46 +0900

    promise

---
+++ async/callback-to-promise.js
@@ -0,0 +1,38 @@
+class UserStorage {
+    loginUser(id, password){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (
+                    (id === 'shgur' && password === 'dream') ||
+                    (id === 'corder' && password == 'academy')
+                ){
+                    resolve(id);
+                } else {
+                    reject(new Error('not found'));
+                }
+                
+            },2000);
+        });  
+    }
+
+    getRoles(user){
+        return new Promise((resolve, reject)=> {
+            setTimeout(() => {
+                if (user === 'shgur') {
+                    resolve({ name: 'shgur', role: 'admin'});
+                } else {
+                    reject(new Error('no access'));
+                }
+            }, 1000);
+        });
+    }
+}
+
+const userStorage = new UserStorage();
+const id =prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(id,password)
+.then(userStorage.getRoles)
+.then(user => alert(`hello ${user.name}, you have a ${user.role} role`))
+.catch(console.log);
+
