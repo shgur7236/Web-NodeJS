12970363f33a64f9d20990d35051bcbdde4ec128
shgur7236 <shgurtns7236@naver.com>
2022-04-06 11:28:19 +0900

    POST 방식으로 전송된 데이터 받기

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
     return `
@@ -13,6 +14,7 @@ function templateHTML(title, list, body){
     <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
     </body>
     </html>
@@ -32,7 +34,6 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
     if(pathname === '/'){
         if(queryData.id === undefined){
             fs.readdir('./data', function(error,filelist){
@@ -55,6 +56,38 @@ var app = http.createServer(function(request,response){
         });
         }
         
+    } else if(pathname === '/create'){
+        fs.readdir('./data', function(error,filelist){
+            var description = 'Hello node.js';
+            var title = 'WEB - create';
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `
+            <form action="http://localhost:3000/create_process" method="post">
+                <P><input type="text" name="title" placeholder="title"></P>
+                <P>
+                    <textarea name="description" placeholder="description"></textarea>
+                </P>
+                <p>
+                    <input type="submit" >
+                </p>
+            </form>
+            `);
+            response.writeHead(200);
+            response.end(template);
+        })
+    } else if(pathname === '/create_process'){
+        let body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            console.log(post.title);
+        });
+        response.writeHead(200);
+        response.end('success');
     } else {
         response.writeHead(404);
         response.end('Not found');
